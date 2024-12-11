2d52a8cb9d5f7e508887f587ea7c143d0e465382
Jonas Almeida <jalmeida@mathbiol.org>
2024-12-11 16:33:35 -0500

    restarted dev es6

--- fminsearch.mjs
+++ fminsearch.mjs
@@ -1,8 +1,15 @@
 // moving fminsearch to ES6 - under development!
-console.log(`fminsearch.mjs loaded ${Date()}`)
+console.log(`fminsearch.mjs imported \n${Date()}`);
 
-const loaded = Date()
+// illustrative examples of fun
 
-export{
-    loaded
+function logistic(x,P){
+    return x.map(
+        xi => 1 / (1 + Math.exp(-(P[0] + (P[1] * xi))))
+    )
+}
+
+
+export {
+    logistic
 }
